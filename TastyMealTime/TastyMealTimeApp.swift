d49eb710dbd30b1723b45a5923701b2474d53152
Mark Nair <marknair@me.com>
2023-11-26 21:41:04 -0600

    Initial Commit

---
+++ TastyMealTime/TastyMealTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TastyMealTimeApp.swift
+//  TastyMealTime
+//
+//  Created by Mark Nair on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TastyMealTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
